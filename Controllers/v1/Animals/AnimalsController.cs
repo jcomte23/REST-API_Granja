5897b4b2f6ef42217bc36c4dd7ca188429432f4a
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-09-16 20:13:45 -0500

    feat: creacion del controlador animal

---
+++ Controllers/v1/Animals/AnimalsController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using REST_API_Granja.Data;
+using REST_API_Granja.Models;
+
+namespace REST_API_Granja.Controllers.v1.Animals
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AnimalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Animal>>> GetAnimals()
+        {
+            return await _context.Animals.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Animal>> GetAnimal(int id)
+        {
+            var animal = await _context.Animals.FindAsync(id);
+
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            return animal;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAnimal(int id, Animal animal)
+        {
+            if (id != animal.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(animal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AnimalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Animal>> PostAnimal(Animal animal)
+        {
+            _context.Animals.Add(animal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAnimal", new { id = animal.Id }, animal);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimal(int id)
+        {
+            var animal = await _context.Animals.FindAsync(id);
+            if (animal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Animals.Remove(animal);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AnimalExists(int id)
+        {
+            return _context.Animals.Any(e => e.Id == id);
+        }
+    }
+}
